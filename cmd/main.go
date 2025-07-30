509602f3c735c9a16e7866fd41770743c722a163
sammyklan3 <sammyklane7@gmail.com>
2025-07-30 16:58:51 +0300

    Core: Add functions

--- cmd/main.go
+++ cmd/main.go
@@ -3,21 +3,16 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/big"
 
 	"base-devnode/client"
 )
 
 func main() {
-	rpc := "https://rpc.scroll.io"
+	rpc := "https://base.llamarpc.com"
 
 	cli, err := client.NewBaseClient(rpc)
 	if err != nil {
 		log.Fatalf("Failed to connect: %v", err)
 	}
-
-	chain, err := cli.GetChainID()
-	if err != nil {
-		log.Fatalf("Failed to get chain ID: %v", err)
-	}
-	fmt.Printf("Connected to chain ID: %s\n", chain.String())
 }
