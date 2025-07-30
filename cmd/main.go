e864c556e3053d0e4ff34fa367ffd057904a09d2
sammyklan3 <sammyklane7@gmail.com>
2025-07-30 17:03:57 +0300

    Core: Add function to fetch block by hash

--- cmd/main.go
+++ cmd/main.go
@@ -3,16 +3,26 @@ package main
 import (
 	"fmt"
 	"log"
-	"math/big"
 
 	"base-devnode/client"
 )
 
 func main() {
-	rpc := "https://base.llamarpc.com"
+	rpc := "https://mainnet.base.org"
 
 	cli, err := client.NewBaseClient(rpc)
 	if err != nil {
 		log.Fatalf("Failed to connect: %v", err)
 	}
+
+	blockHash := "0x08322071401b3a69b00a956560393c57beaac47c7dad5adc2feae8b8f13cc253"
+
+	block, err := cli.GetBlockByHash(blockHash)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Block Number: %s\n", block.Number().String())
+	fmt.Printf("Block Hash: %s\n", block.Hash().Hex())
+	fmt.Printf("Block Transactions: %d\n", len(block.Transactions()))
 }
